d3c17d5be3b30d6c0a7e204aa147a82b4853eab9
Chloe Allan <ca14g14@soton.ac.uk>
2017-02-11 14:15:09 +0000

    changed design of homepage, added in pages for everything and got general look

--- public/javascripts/controllers/index.js
+++ public/javascripts/controllers/index.js
@@ -47,12 +47,26 @@ angular.module('app', ['ngRoute', 'ngResource', 'ngMaterial', 'ngCookies'])
     //---------------
 
     .controller('HeaderController', ['$scope', 'userPersistence', '$location', function($scope, userPersistence, $location){
-        $scope.$on('$routeChangeStart', function (next, current) {
-            console.log(userPersistence.getCookieData());
-            if(userPersistence.getCookieData() === undefined){
-                $location.url('/');
-            }
-        });
+        //TODO: need to change as when user creates account they cant go to create account page as this is stopping it which is wrong
+        // $scope.$on('$routeChangeStart', function (next, current) {
+        //     console.log(userPersistence.getCookieData());
+        //     if(userPersistence.getCookieData() === undefined){
+        //         $location.url('/');
+        //     }
+        // });
+        $scope.DashboardClick = function(){
+            $location.url('/dashboard');
+        };
+        $scope.MentoringClick = function(){
+            $location.url('/mentor');
+        };
+        $scope.GroupsClick = function(){
+            $location.url('/group');
+        };
+        $scope.ProfileClick = function(){
+            console.log('click');
+            $location.url('/profile');
+        };
 
         $scope.isLoggedIn = function(){
             if(userPersistence.getCookieData() !== undefined){
@@ -215,7 +229,23 @@ angular.module('app', ['ngRoute', 'ngResource', 'ngMaterial', 'ngCookies'])
 
     .controller('HomepageController', ['$scope', '$location', '$http', 'userPersistence',
         function($scope, $location, $http, userPersistence) {
-            // $scope.username = userPersistence.getCookieData();
+            $scope.menu = [
+                {
+                    link : '',
+                    title: 'Dashboard',
+                    icon: 'dashboard'
+                },
+                {
+                    link : '',
+                    title: 'Friends',
+                    icon: 'group'
+                },
+                {
+                    link : '',
+                    title: 'Messages',
+                    icon: 'message'
+                }
+            ];
     }])
 
     .controller('UserprofileController', ['$scope', '$location', '$http', 'userPersistence',
@@ -227,6 +257,115 @@ angular.module('app', ['ngRoute', 'ngResource', 'ngMaterial', 'ngCookies'])
         function($scope, $location, $http, userPersistence) {
 
     }])
+
+    .controller('GroupController', ['$scope', '$location', '$http', 'userPersistence',
+        function($scope, $location, $http, userPersistence) {
+
+    }])
+
+    .controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog', function($scope, $mdBottomSheet, $mdSidenav, $mdDialog){
+        $scope.toggleSidenav = function(menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+        $scope.menu = [
+            {
+                link : '',
+                title: 'Dashboard',
+                icon: 'dashboard'
+            },
+            {
+                link : '',
+                title: 'Friends',
+                icon: 'group'
+            },
+            {
+                link : '',
+                title: 'Messages',
+                icon: 'message'
+            }
+        ];
+        $scope.admin = [
+            {
+                link : '',
+                title: 'Trash',
+                icon: 'delete'
+            },
+            {
+                link : 'showListBottomSheet($event)',
+                title: 'Settings',
+                icon: 'settings'
+            }
+        ];
+        $scope.activity = [
+            {
+                what: 'Brunch this weekend?',
+                who: 'Ali Conners',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                what: 'Summer BBQ',
+                who: 'to Alex, Scott, Jennifer',
+                when: '3:08PM',
+                notes: "Wish I could come out but I'm out of town this weekend"
+            },
+            {
+                what: 'Oui Oui',
+                who: 'Sandra Adams',
+                when: '3:08PM',
+                notes: "Do you have Paris recommendations? Have you ever been?"
+            },
+            {
+                what: 'Birthday Gift',
+                who: 'Trevor Hansen',
+                when: '3:08PM',
+                notes: "Have any ideas of what we should get Heidi for her birthday?"
+            },
+            {
+                what: 'Recipe to try',
+                who: 'Brian Holt',
+                when: '3:08PM',
+                notes: "We should eat this: Grapefruit, Squash, Corn, and Tomatillo tacos"
+            },
+        ];
+        $scope.alert = '';
+        $scope.showListBottomSheet = function($event) {
+            $scope.alert = '';
+            $mdBottomSheet.show({
+                template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
+                controller: 'ListBottomSheetCtrl',
+                targetEvent: $event
+            }).then(function(clickedItem) {
+                $scope.alert = clickedItem.name + ' clicked!';
+            });
+        };
+
+        $scope.showAdd = function(ev) {
+            $mdDialog.show({
+                controller: DialogController,
+                template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
+                targetEvent: ev,
+            })
+                .then(function(answer) {
+                    $scope.alert = 'You said the information was "' + answer + '".';
+                }, function() {
+                    $scope.alert = 'You cancelled the dialog.';
+                });
+        };
+    }])
+    .controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
+        $scope.items = [
+            { name: 'Share', icon: 'share' },
+            { name: 'Upload', icon: 'upload' },
+            { name: 'Copy', icon: 'copy' },
+            { name: 'Print this page', icon: 'print' },
+        ];
+
+        $scope.listItemClick = function($index) {
+            var clickedItem = $scope.items[$index];
+            $mdBottomSheet.hide(clickedItem);
+        };
+    })
 
         //---------------
     // Routes - add in accesses to each url
@@ -243,16 +382,20 @@ angular.module('app', ['ngRoute', 'ngResource', 'ngMaterial', 'ngCookies'])
                 controller: 'SignupController'
             })
             .when('/home', {
-                templateUrl: 'partials/homepage2.html',
+                templateUrl: 'partials/homepage.html',
                 controller: 'HomepageController'
             })
             .when('/profile', {
                 templateUrl: 'partials/userprofile.html',
                 controller: 'UserprofileController'
             })
-            .when('/partner',{
-                templateUrl: 'partials/workpartner.html',
+            .when('/mentor', {
+                templateUrl: 'partials/mentor.html',
                 controller: 'WorkpartnerController'
+            })
+            .when('/group',  {
+                templateUrl: 'partials/group.html',
+                controller: 'GroupController'
             })
             .otherwise({
                 redirectTo: '/'
